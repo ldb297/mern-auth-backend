e5d599801fbd17e0f73e7b8867f62a2d5b5f40ab
Leonidas <ldbrooks297@gmail.com>
2021-03-04 15:01:02 -0800

    added login creation route with auth

--- controllers/user.js
+++ controllers/user.js
@@ -26,7 +26,7 @@ const register = (req,res) =>{
         if(user){
             return res.status(400).json({message: 'Email already exists'})
         } else {
-            const newUser = new User({
+            const newUser = new db.User({
                 name: req.body.name,
                 email: req.body.email,
                 password: req.body.password
@@ -51,6 +51,51 @@ const register = (req,res) =>{
     })
 }
 
+const login = async (req,res) => {
+    //post to find user, authenticate, and return
+    console.log(`>>>>>>> inside /login`)
+    console.log(`>>>>>>> req.body`)
+    console.log(req.body)
+
+    const foundUser = await db.User.findOne({ email: req.body.email })
+
+    if(foundUser){
+        // user has been located in database
+        let isMatch =  await bcrypt.compare(req.body.password, foundUser.password)
+        console.log(isMatch)
+        if (isMatch){
+        // if user is matched, then we want to send a json web token
+        // create a token payload 
+        // add an expiredToken = Date.now()
+        // save the user
+            const payload = {
+                id: foundUser.id,
+                email: foundUser.email,
+                name: foundUser.name
+            }
+
+        //setting hourly token expiration
+            jwt.sign(payload, JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
+                if(err) {
+                    // 3600s token has expired
+                    res.status(400).json({ message: 'Session has ended, please log in again'})
+                }
+                // a bearer token to check that user's login token is still valid with each req
+                const legit = jwt.verify(token, JWT_SECRET, { expiresIn: 60 })
+                console.log(`>>>>>>>> legit`)
+                console.log(`${legit}`)
+                res.json({ success: true, token: `Bearer ${token}`, userData: legit})
+            })
+        } else {
+            //this is the else statement for isMatch
+            return res.status(400).json({ message: 'Email or Password incorrect'})
+        }
+        //this is the else statement for foundUser
+    } else {
+        return res.status(400).json({ message: 'User not found'})
+    }
+}
+
 //exports 
 
-module.exports = { test, register }
+module.exports = { test, register, login }
